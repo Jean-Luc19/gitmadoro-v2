f9014a5f3a983296bb4d6ff5fff7b2d7373712c3
Aaron Gottlieb <atticus19@gmail.com>
2017-06-29 16:44:10 -0600

    adds icons to configurepommodal component

--- client/src/components/modals/configure-pom-modal.jsx
+++ client/src/components/modals/configure-pom-modal.jsx
@@ -6,14 +6,27 @@ import ModalWrapper from './modal-wrapper';
 import { openModal } from '../../actions';
 import { CLOCK_MODAL } from './modal-types';
 
-const ConfigurePomModal = props => {
+class ConfigurePomModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  return (
-    <ModalWrapper title="Set Up">
-      <div>Set the duration and goal of your pom session</div>
-      <button onClick={() => props.openModal(CLOCK_MODAL)}>Start The Pom</button>
-    </ModalWrapper>
-  );
+    };
+  }
+
+  render() {
+    return (
+      <ModalWrapper title="Set Up">
+        <div>Set the duration and goal of your pom session</div>
+        <div className="pom-settings">
+          <i className="material-icons settings-icons">replay_30</i>
+          <h1>25:00</h1>
+          <i className="material-icons settings-icons">forward_30</i>
+        </div>
+        <button onClick={() => this.props.openModal(CLOCK_MODAL)}>Start The Pom</button>
+      </ModalWrapper>
+    );
+  }
 };
 
 export default connect(null, { openModal })(ConfigurePomModal);
